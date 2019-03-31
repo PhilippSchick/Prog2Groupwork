9a54c57068df7ef04b255f9376803ac9ee83e478
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-03-31 17:50:26 +0200

    Added Comments to all Methods

--- src/de/hsmannheim/ostereiproduktion/Huehnerfarm.java
+++ src/de/hsmannheim/ostereiproduktion/Huehnerfarm.java
@@ -6,26 +6,59 @@ import java.util.ArrayList;
  * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
  * @version 1.00, 27.03.2019
  *
+ *          Generates instances of {@link Ei}
  */
 public class Huehnerfarm {
+	/**
+	 * Minimal number of {@link Ei} returned
+	 */
+	private int minBest;
+	/**
+	 * Maximal number of {@link Ei} returned
+	 */
+	private int maxBest;
 
-	public Huehnerfarm() {
-		
+	/**
+	 * Generates a new instance of {@link Huehnerfarm} and sets the minimal and the
+	 * maximal number of {@link Ei} returned
+	 * 
+	 * @param minBest Minimal number of {@link Ei} returned
+	 * @param maxBest Maximal number of {@link Ei} returned
+	 */
+	public Huehnerfarm(int minBest, int maxBest) {
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @param anzEier
-	 * @return ArrayList<Ei[]>
+	 * Generates a new instance of {@link Huehnerfarm} and sets the minimal number
+	 * of {@link Ei} returned to 25 and the maximal to 5000
 	 */
-	public ArrayList<Ei[]> erstelleEierkartonstapel(int anzEier) {
+	public Huehnerfarm() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Returns a pack of Eierkartons, a Eierkarton is represented by a Array of
+	 * {@link Ei} with size 25
+	 * 
+	 * @param anzEier The number of Eiern contain in the pack
+	 * @return ArrayList<Ei[]> The Eierkarton pack
+	 */
+	private ArrayList<Ei[]> erstelleEierkartonstapel(int anzEier) {
 		return null;
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @param menge
-	 * @param ausschuss
-	 * @return ArrayList<Ei[]>
+	 * Returns a pack of Eierkartons, a Eierkarton is represented by a Array of
+	 * {@link Ei} with size 25
+	 * 
+	 * @param menge     The number of {@link Ei} returned, has to be between
+	 *                  <code>minBest</code> and <code>maxBest</code>
+	 * @param ausschuss The percentage of {@link Ei} where <code>isDefekt()</code>
+	 *                  equals <code>true</code>, has to be less or equal 100 and
+	 *                  bigger than 0
+	 * @return ArrayList<Ei[]> The Eierkarton pack
 	 */
 	public ArrayList<Ei[]> liefereEier(int menge, int ausschuss) {
 		return null;
@@ -33,9 +66,12 @@ public class Huehnerfarm {
 	}
 
 	/**
-	 * @param menge
-	 * @param 
-	 * @return ArrayList<Ei[]>
+	 * Returns a pack of Eierkartons, a Eierkarton is represented by a Array of
+	 * {@link Ei} with size 25.
+	 * 
+	 * @param menge The number of {@link Ei} returned, has to be between
+	 *              <code>minBest</code> and <code>maxBest</code>
+	 * @return ArrayList<Ei[]> The Eierkarton pack
 	 */
 	public ArrayList<Ei[]> liefereEier(int menge) {
 		return null;
@@ -43,4 +79,3 @@ public class Huehnerfarm {
 	}
 
 }
-
