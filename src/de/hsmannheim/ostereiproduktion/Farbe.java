f659e0c942b8fbae35dab33e6ef34d7dac7bda68
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-03-31 22:53:10 +0200

    Added comments to Farbe

--- src/de/hsmannheim/ostereiproduktion/Farbe.java
+++ src/de/hsmannheim/ostereiproduktion/Farbe.java
@@ -3,6 +3,7 @@ package de.hsmannheim.ostereiproduktion;
  * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
  * @version 1.00, 27.03.2019
  *
+ * Represents the color of an {@link Ei}
  */
 public enum Farbe {
 	blau, gelb, grün, rot, ugly
