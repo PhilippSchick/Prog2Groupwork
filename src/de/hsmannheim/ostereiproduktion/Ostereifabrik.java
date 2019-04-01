32dc801a56d2e16b164331becbed361eb6844803
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-04-01 18:44:29 +0200

    Added comments to Ostereifabrik

--- src/de/hsmannheim/ostereiproduktion/Ostereifabrik.java
+++ src/de/hsmannheim/ostereiproduktion/Ostereifabrik.java
@@ -6,37 +6,48 @@ import java.util.ArrayList;
  * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
  * @version 1.00, 27.03.2019
  *
- * Converts {@link Ei} to {@link Osterei}
+ *          Converts {@link Ei} to {@link Osterei}
  */
 public class Ostereifabrik {
 
 	/**
-	 * @param gestapelteEierkartons
-	 * @return void
+	 * Receives {@link Ei}er to convert them in {@link Osterei}er, if there are
+	 * already are {@link Ei}er in the Factory all new Eggs will be destroyed
+	 * 
+	 * @param gestapelteEierkartons The {@link Ei}er to process
 	 */
 	public void wareneingang(ArrayList<Ei[]> gestapelteEierkartons) {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @param kartonId
-	 * @return void
+	 * Cooks a Eierkarton and converts its {@link Ei}er to {@link Osterei}ern. If an
+	 * {@link Ei} is already cooked or is defect, the whole Eierkarton will be
+	 * destroyed
+	 * 
+	 * @param kartonId The id of the Eierkarton to process
 	 */
 	private void kocheEier(int kartonId) {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @param kartonId
-	 * @param farbe
-	 * @return void
+	 * Sets the color of a {@link Osterei}. If a {@link Osterei} is already colored
+	 * a {@link EggColoredException} is thrown, if a {@link Osterei} is still raw
+	 * (means it is just a {@link Ei}) a {@link EggNotBoiledException} will be
+	 * thrown
+	 * 
+	 * @param kartonId The id of the Eierkarton to process
+	 * @param farbe    The color of the {@link Osterei}ern
 	 */
 	private void faerbeEier(int kartonId, Farbe farbe) {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @return int
+	 * Returns the Number of Eierkartons on the pack
+	 * 
+	 * @return The Number of Eierkartons
 	 */
 	public int getAnzahlEierkartons() {
 		return 0;
@@ -44,19 +55,20 @@ public class Ostereifabrik {
 	}
 
 	/**
-	 * @return void
+	 * Prints a representation of the Eierkarton pack with the colors of the
+	 * {@link Ei}ern on the console
 	 */
 	public void printGestapelteEierkartons() {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @param kartonId
-	 * @return void
+	 * Removes a Karton from the pack
+	 * 
+	 * @param kartonId The id of the Karton, has to be greater or equals 0
 	 */
 	public void vernichteKarton(int kartonId) {
 		// TODO Auto-generated method stub
 	}
 
 }
-
