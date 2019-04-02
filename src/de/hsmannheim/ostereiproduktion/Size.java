26b23a8cdcdf438a9625850180e2b82f44b62966
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-04-02 14:41:13 +0200

    Added Size

---
+++ src/de/hsmannheim/ostereiproduktion/Size.java
@@ -0,0 +1,21 @@
+package de.hsmannheim.ostereiproduktion;
+
+/**
+ * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
+ * @version 1.00, 02.04.2019
+ *
+ *          Represents the Size of a {@link Ei}
+ */
+public enum Size {
+	S, M, L, XL;
+
+	/**
+	 * Returns the matching Size for a given Integer
+	 * 
+	 * @param größe The size as an Integer
+	 * @return The Size
+	 */
+	public static Size getSize(int size) {
+		return null;
+	}
+}
