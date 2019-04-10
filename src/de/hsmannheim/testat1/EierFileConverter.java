2782268430130d240a650f8d9d17582a693e8413
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-04-10 11:54:05 +0200

    Added EierFileConverter

---
+++ src/de/hsmannheim/testat1/EierFileConverter.java
@@ -0,0 +1,46 @@
+package de.hsmannheim.testat1;
+
+import uebung1a.Ei;
+
+/**
+ * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
+ * 
+ *         Provides a Converter from {@link Ei} to a File and Reverse
+ *
+ */
+public class EierFileConverter {
+	/**
+	 * Converts Eierkartonstapel to a File
+	 * 
+	 * @param eierkartonstapel The Eierkartonstapel
+	 * @param filename         The name of the file
+	 */
+	public void eierToFile(Ei[][] eierkartonstapel, String filename) {
+
+	}
+
+	/**
+	 * Converts a File to a Eierkartonstapel
+	 * 
+	 * @param filename The name of the file
+	 * @return The Eierkartonstapel
+	 */
+	public Ei[][] fileToEier(String filename) {
+		return null;
+	}
+	
+	/**
+	 * Converts a {@link Ei} to a String
+	 * 
+	 * @param ei The {@link Ei}
+	 * @return The String representing the {@link Ei}
+	 */
+	private String eiToString(Ei ei) {
+		return null;
+	}
+	
+	private Ei stringToEi(String s) {
+		return null;
+	}
+
+}
