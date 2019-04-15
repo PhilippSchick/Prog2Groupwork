602d0edad87e70560832fd87c795cbcc32a5c7dd
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-04-15 15:38:06 +0200

    Generated InputModul

---
+++ src/de/hsmannheim/testat1/InputModul.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package de.hsmannheim.testat1;
+
+/**
+ * @author philipp
+ * @version 1.00, 15.04.2019
+ *
+ */
+public class InputModul implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
