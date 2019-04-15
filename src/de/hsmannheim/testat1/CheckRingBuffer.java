8411c117207fc8e6d5f7692e7f74585d4d3d5e1e
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-04-15 18:53:19 +0200

    Added attributes and method stubs

--- src/de/hsmannheim/testat1/CheckRingBuffer.java
+++ src/de/hsmannheim/testat1/CheckRingBuffer.java
@@ -3,6 +3,10 @@
  */
 package de.hsmannheim.testat1;
 
+import java.nio.BufferUnderflowException;
+
+import uebung1a.Ei;
+
 /**
  * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
  * @version 1.00, 15.04.2019
@@ -11,5 +15,57 @@ package de.hsmannheim.testat1;
  *          Methods should be Thread save
  */
 public class CheckRingBuffer {
+	/**
+	 * Keeps the Eggs
+	 */
+	private Ei[] buffer = new Ei[50];
+
+	/**
+	 * The Output-Position of the Buffer
+	 */
+	private int outPointer;
+
+	/**
+	 * The Input-Position of the Buffer
+	 */
+	private int inPointer;
+
+	/**
+	 * The current Check Position
+	 */
+	private int checkPosition;
+
+	/**
+	 * Adds a new Egg to the Buffer
+	 * 
+	 * @param input The Egg to put in the Buffer
+	 */
+	public void enqueue(Ei input) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Returns a Egg from the Buffer
+	 * @throws BufferUnderflowException if there is no Egg to return
+	 * 
+	 * @return A Egg
+	 */
+	public Ei dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Checks the Egg on the Check-Position, if it's defect it will return false and
+	 * deletes the Egg
+	 * 
+	 * @throws BufferUnderflowException if there isn't a Egg to Check
+	 * 
+	 * @return true if the Egg isn't defect
+	 */
+	public boolean checkEgg() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
