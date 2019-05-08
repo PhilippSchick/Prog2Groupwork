dabba4ba870c1834723c7f97d706fd948b51056b
Philipp Schick <philipp.schick@stud.hs-mannheim.de>
2019-05-08 11:55:24 +0200

    Added Main_2

---
+++ src/de/hsmannheim/ArcheProject/Main_2.java
@@ -0,0 +1,26 @@
+package de.hsmannheim.ArcheProject;
+
+/**
+ * @author Jeremias Kunz, Daniel Reichel, Philipp Schick
+ * 
+ *         The Main Program for the second assignment
+ *
+ */
+public class Main_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AnimalQueue queue = new AnimalQueue(20);
+		queue.printWarteschlange();
+
+		Arche arch = new Arche();
+		for (Tier animal : queue) {
+			arch.add(animal);
+		}
+		arch.printStallbelegung();
+
+	}
+
+}
